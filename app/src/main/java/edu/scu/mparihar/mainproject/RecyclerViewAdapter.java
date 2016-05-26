0659579f1414155eeb7ee737f4601d77e389162f
manoj2509 <manojparihar09@gmail.com>
2016-05-25 23:42:35 -0700

    Adding RecyclerView

---
+++ app/src/main/java/edu/scu/mparihar/mainproject/RecyclerViewAdapter.java
@@ -0,0 +1,29 @@
+package edu.scu.mparihar.mainproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Mj on 25-May-16.
+ */
+public class RecyclerViewAdapter extends RecyclerView.Adapter {
+    public RecyclerViewAdapter(List<eventData> demoData) {
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
